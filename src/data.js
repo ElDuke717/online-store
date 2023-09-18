aa7ece4a5eae7b62c02f1a8665c2bee85b82acc8
elduke717 <spacematcha@proton.me>
2023-09-18 16:07:29 -0400

    add images for products

---
+++ src/data.js
@@ -0,0 +1,88 @@
+const products = [
+  {
+    id: "1",
+    name: "Laptop",
+    price: 999,
+    image: "/assets/images/laptop.png",
+    description: "High-performance laptop.",
+  },
+  {
+    id: "2",
+    name: "Smartphone",
+    price: 499,
+    image: "/assets/images/smartphone.png",
+    description: "Latest model with great features.",
+  },
+  {
+    id: "3",
+    name: "Camera",
+    price: 549,
+    image: "/assets/images/camera.jpg",
+    description: "DSLR with great features.",
+  },
+  {
+    id: "4",
+    name: "Headphones",
+    price: 299,
+    image: "/assets/images/headphones.png",
+    description: "Noise-cancelling headphones.",
+  },
+  {
+    id: "5",
+    name: "Smart Watch",
+    price: 199,
+    image: "/assets/images/smartwatch.jpg",
+    description: "Smart watch with heart rate monitor.",
+  },
+  {
+    id: "6",
+    name: "Speaker",
+    price: 129,
+    image: "/assets/images/speaker.png",
+    description: "Bluetooth speaker with high-quality sound.",
+  },
+  {
+    id: "7",
+    name: "Tablet",
+    price: 599,
+    image: "/assets/images/tablet.png",
+    description: "Tablet with 10-inch screen.",
+  },
+  {
+    id: "8",
+    name: "Keyboard",
+    price: 79,
+    image: "/assets/images/keyboard.png",
+    description: "Wireless mechanical keyboard.",
+  },
+  {
+    id: "9",
+    name: "Mouse",
+    price: 49,
+    image: "/assets/images/mouse.png",
+    description: "Wireless optical mouse.",
+  },
+  {
+    id: "10",
+    name: "Monitor",
+    price: 199,
+    image: "/assets/images/monitor.png",
+    description: "24-inch computer monitor.",
+  },
+  {
+    id: "11",
+    name: "Printer",
+    price: 89,
+    image: "/assets/images/printer.png",
+    description: "Wireless all-in-one printer.",
+  },
+  {
+    id: "12",
+    name: "Router",
+    price: 69,
+    image: "/assets/images/router.png",
+    description: "Wireless router with four Ethernet ports.",
+  },
+];
+
+export default products;
